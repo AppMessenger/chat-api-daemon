e613cfbfe5a108848f5efa933af8e756ca395903
mawalu <web@mawalabs.de>
2016-03-08 21:15:12 +0100

    Started to implement events

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
+var events = require('events');
 var spawn = require('child_process').spawn;
 var client = spawn('php', ['client.php', process.env.NUMBER, process.env.NICKNAME, process.env.PASSWORD], {cwd: '../client'});
 
+var whatsapp = new events.EventEmitter();
+
 client.stdout.on('data', function (data) {
   try {
-    var decoded = data.toString().trim().split('\r\n').map(JSON.parse);
-    decoded.forEach(function(e) {
-      console.log(e.method)
+    data.toString().trim().split('\r\n').map(JSON.parse).forEach(function(e) {
+      whatsapp.emit('whatsapp_event', e.method, e.args);
     });
+
   } catch (e) {
     console.log(data.toString().trim());
     console.log('Bad controll msg');
@@ -16,3 +19,8 @@ client.stdout.on('data', function (data) {
 client.on('close', function (code)  {
   console.log('child process exited');
 });
+
+whatsapp.on('whatsapp_event', function(method, args) {
+  console.log(method);
+  console.log(args);
+})
