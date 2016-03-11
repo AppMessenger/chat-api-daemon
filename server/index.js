c52cbf01b45d38ee205762a1791f3e88eb5b61f4
mawalu <web@mawalabs.de>
2016-03-11 17:52:01 +0100

    First version of websocket api provider

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 var whatsapp = require('./src/whatsapp.js');
+var websockets = require('./src/websockets.js');
 
 whatsapp.init(process.env.NUMBER, process.env.NICKNAME, process.env.PASSWORD);
+
+websockets.init(8080, whatsapp.EventEmitter);
 
 whatsapp.EventEmitter.on('whatsapp_event', function(method, args) {
   console.log(method);
   console.log(args);
 })
+
+setTimeout(function() {
+  whatsapp.EventEmitter.emit('send_command', 'sendMessage', ['4915253602422', 'hello world']);
+}, 5000);
---
+++ server/src/websockets.js
@@ -0,0 +1,27 @@
+var WebSocketServer = require('ws').Server;
+
+var websockets = {}
+
+websockets.init = function(port, EventEmitter) {
+  websockets.EventEmitter = EventEmitter;
+  websockets.wss = new WebSocketServer({ port: port });
+
+  websockets.wss.on('connection', websockets.handleConnection);
+  websockets.EventEmitter.on('whatsapp_event', websockets.handleWhatsappEvent);
+}
+
+websockets.handleConnection = function(ws) {
+  ws.on('message', function(message) {
+    console.log(message);
+    message = JSON.parse(message);
+    websockets.EventEmitter.emit('send_command', message.method, message.args);
+  })
+}
+
+websockets.handleWhatsappEvent = function(method, args) {
+  websockets.wss.clients.forEach(function(client) {
+    client.send(JSON.stringify({method: method, args: args}));
+  });
+}
+
+module.exports = websockets;
